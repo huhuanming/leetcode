aadb667811b15f175f7346778f7f788dfe96f335
hu <workboring@gmail.com>
2016-09-09 18:46:08 +0800

    add swift

---
+++ Swift/LeetCode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LeetCode
+//
+//  Created by 胡 桓铭 on 16/9/5.
+//  Copyright © 2016年 胡 桓铭. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift/LeetCode/ReverseString.swift
@@ -0,0 +1,12 @@
+// 344. Reverse String
+// Write a function that takes a string as input and returns the string reversed.
+
+// Example:
+// Given s = "hello", return "olleh".
+
+import Foundation
+
+
+func reverse_string(s:String)->String{
+    return String(s.characters.reverse())
+}
